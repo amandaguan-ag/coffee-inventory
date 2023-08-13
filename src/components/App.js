09354f05bfdcdff862006170dec3bf1e7bac23ab
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-12 23:46:52 -0700

    Add CoffeeBeanMenu

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import CoffeeBeanMenu from "./CoffeeBeanMenu"; // Import the CoffeeBeanMenu component
 import InventoryControl from "./InventoryControl";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <CoffeeBeanMenu /> {/* Include the CoffeeBeanMenu component */}
+      {/* Other components or content can go here */}
       <InventoryControl />
     </React.Fragment>
   );
