73c54ee1033b0ba928ce8bb12b72b3491c340591
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-12 18:14:35 -0700

    add form

--- src/components/NewInventoryForm.js
+++ src/components/NewInventoryForm.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-function NewInventoryForm(props) {
+function NewInventoryForm() {
+  function handleNewInventoryFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.origin.value);
+    console.log(event.target.price.value);
+    console.log(event.target.roast.value);
+    console.log(event.target.remaining.value);
+  }
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewInventoryFormSubmission}>
+        <input type="text" name="name" placeholder="Bean Names" />
+        <input type="text" name="origin" placeholder="Bean Origin" />
+        <input type="text" name="price" placeholder="Bean price" />
+        <input type="text" name="roast" placeholder="Bean rosat" />
+        <input type="text" name="remaining" placeholder="Bean remaining" />
+        <button type="submit">Add</button>
+      </form>
     </React.Fragment>
   );
 }
