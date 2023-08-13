69c4b8005ab3766e00414537fdbfd60e554e7231
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-13 00:32:17 -0700

    fix inventory detail

--- src/components/InventoryDetail.js
+++ src/components/InventoryDetail.js
@@ -4,17 +4,20 @@ import PropTypes from "prop-types";
 function InventoryDetail(props) {
   const { inventory, onClickingDelete, onClickingEdit } = props;
 
+  // Destructure the inventory object to get the properties
+  const { name, origin, price, roast, remaining } = inventory;
+
   return (
     <React.Fragment>
       <h1>Inventory Detail</h1>
       <h3>
-        {props.name} - {props.origin}
+        {name} - {origin}
       </h3>
       <p>
-        Price: <em>${props.price}</em>
+        Price: <em>${price}</em>
       </p>
-      <p>Roast: {props.roast}</p>
-      <p>Remaining: {props.remaining} pounds</p>
+      <p>Roast: {roast}</p>
+      <p>Remaining: {remaining} pounds</p>
       <button onClick={onClickingEdit}>Update Inventory</button>
       <button onClick={() => onClickingDelete(inventory.id)}>
         Close Inventory
