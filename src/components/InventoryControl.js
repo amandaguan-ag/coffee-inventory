8c977600743c35c68ae6d650acfc37d0c73fb31c
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-12 17:59:14 -0700

    add local state

---
+++ src/components/InventoryControl.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NewInventoryForm from "./NewInventoryForm";
+import InventoryList from "./InventoryList";
+
+class InventoryControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+    };
+  }
+
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+
+export default InventoryControl;
