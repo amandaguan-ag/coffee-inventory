2037b044aebd4501767285d7e4b6bb7cb8966419
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-12 17:44:59 -0700

    create an array of inventory

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,24 +1,45 @@
 import React from "react";
 import Inventory from "./Inventory";
 
+const mainInventoryList = [
+  {
+    name: "Ethiopian Yirgacheffe",
+    origin: "Ethiopia",
+    price: "18.99",
+    roast: "medium",
+    remaining: "130",
+  },
+  {
+    name: "Colombian Supremo",
+    origin: "Colombia",
+    price: "16.50",
+    roast: "dark",
+    remaining: "130",
+  },
+  {
+    name: "Sumatra Mandheling",
+    origin: "Indonesia",
+    price: "17.25",
+    roast: "dark",
+    remaining: "130",
+  },
+];
+
 function InventoryList() {
   return (
-    <div>
-      <Inventory
-        name="Ethiopian Yirgacheffe"
-        origin="Ethiopia"
-        price="18.99"
-        roast="medium"
-        remaining="130"
-      />
-      <Inventory
-        name="Colombian Supremo"
-        origin="Colombia"
-        price="16.50"
-        roast="dark"
-        remaining="130"
-      />
-    </div>
+    <React.Fragment>
+      <hr />
+      {mainInventoryList.map((inventory, index) => (
+        <Inventory
+          names={inventory.names}
+          origin={inventory.origin}
+          price={inventory.price}
+          roast={inventory.roast}
+          remaining={inventory.remaining}
+          key={index}
+        />
+      ))}
+    </React.Fragment>
   );
 }
 
