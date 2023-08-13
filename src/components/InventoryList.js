f6843396d1334ad1da7ab8b35523aed6070a1427
Amanda Guan <ag.amandaguan@gmail.com>
2023-08-12 17:34:28 -0700

    Add another inventory

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -3,13 +3,22 @@ import Inventory from "./Inventory";
 
 function InventoryList() {
   return (
-    <Inventory
-      name="Ethiopian Yirgacheffe"
-      origin="Ethiopia"
-      price="18.99"
-      roast="medium"
-      remaining="130"
-    />
+    <div>
+      <Inventory
+        name="Ethiopian Yirgacheffe"
+        origin="Ethiopia"
+        price="18.99"
+        roast="medium"
+        remaining="130"
+      />
+      <Inventory
+        name="Colombian Supremo"
+        origin="Colombia"
+        price="16.50"
+        roast="dark"
+        remaining="130"
+      />
+    </div>
   );
 }
 
